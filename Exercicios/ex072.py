edc5b4f45f420847cc3075cdb64fe83b5b308a1a
FranciscaSolinho <244995175+FranciscaSolinho@users.noreply.github.com>
2025-11-25 17:30:34 +0000

    Update ex072.py

--- Exercicios/ex072.py
+++ Exercicios/ex072.py
@@ -1,6 +1,7 @@
 #Crie um programa que tenha uma função que receba 3 parâmetros:
-#inicio, fim e passo.
+#inicio, fim e passo (tipo 1 3 5 7).
 #O programa deve realizar 3 contagens através da função.
     #a) De 1 até 20, de 2 em 2;
     #b) De 10 até 0, de 1 em 1;
     #c) Contagem personalizada.
+
